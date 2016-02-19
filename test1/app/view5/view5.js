6f644d17ca0dcdeea7dac72dae0dac1eeccb060c
zhangzxc <6842942@qq.com>
2016-02-19 17:26:06 +0800

    Rejected commit: Default

--- test1/app/view5/view5.js
+++ test1/app/view5/view5.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view5', ['ngRoute'])
+angular.module('myApp.view5', ['ngRoute', 'ui.bootstrap'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view5', {
@@ -9,10 +9,511 @@ angular.module('myApp.view5', ['ngRoute'])
         });
     }])
 
-    .controller('View5Ctrl', function ($scope, $timeout, $interval, $http) {
-        $scope.title = '标题';
+    .controller('View5Ctrl', function ($scope, $timeout, $interval, $http, $uibModal,$window, $uibPosition,$sce) {
+        $scope.title = '点击标题';
         $scope.text = '展开内容';
 
+        //展开/收缩
+        $scope.oneAtATime = true;
+        $scope.isCollapsed = false;
+        $scope.groups = [
+            {
+                title: '动态标题 - 1',
+                content: '动态内容 - 1'
+            },
+            {
+                title: '动态标题 - 2',
+                content: '动态内容 - 2'
+            }
+        ];
+
+        $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+        $scope.addItem = function () {
+            var newItemNo = $scope.items.length + 1;
+            $scope.items.push('Item ' + newItemNo);
+        };
+
+        $scope.status = {
+            isFirstOpen: true,
+            isFirstDisabled: false
+        };
+
+        //展开收缩 END
+
+
+        //ALERT例子
+        $scope.alerts = [
+            {type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.'},
+            {type: 'success', msg: 'Well done! You successfully read this important alert message.'}
+        ];
+
+        $scope.addAlert = function () {
+            $scope.alerts.push({msg: 'Another alert!'});
+        };
+
+        $scope.closeAlert = function (index) {
+            $scope.alerts.splice(index, 1);
+        };
+        //ALERT例子 END
+
+
+        //button 例子
+        $scope.singleModel = 1;
+
+        $scope.radioModel = 'Middle';
+
+        $scope.checkModel = {
+            left: false,
+            middle: true,
+            right: false
+        };
+
+        $scope.checkResults = [];
+
+        $scope.$watchCollection('checkModel', function () {
+            $scope.checkResults = [];
+            angular.forEach($scope.checkModel, function (value, key) {
+                if (value) {
+                    $scope.checkResults.push(key);
+                }
+            });
+        });
+        //button 例子     END
+
+
+
+        //图片显示
+        $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        var slides = $scope.slides = [];
+        var currIndex = 0;
+
+        $scope.addSlide = function() {
+            var newWidth = slides.length + 1;
+            slides.push({
+                image: 'images/' + newWidth + '.jpg',
+                text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
+                id: currIndex++
+            });
+        };
+
+        $scope.randomize = function() {
+            var indexes = generateIndexesArray();
+            assignNewIndexesToSlides(indexes);
+        };
+
+        for (var i = 0; i < 4; i++) {
+            $scope.addSlide();
+        }
+
+        // Randomize logic below
+
+        function assignNewIndexesToSlides(indexes) {
+            for (var i = 0, l = slides.length; i < l; i++) {
+                slides[i].id = indexes.pop();
+            }
+        }
+
+        function generateIndexesArray() {
+            var indexes = [];
+            for (var i = 0; i < currIndex; ++i) {
+                indexes[i] = i;
+            }
+            return shuffle(indexes);
+        }
+
+        // http://stackoverflow.com/questions/962802#962890
+        function shuffle(array) {
+            var tmp, current, top = array.length;
+
+            if (top) {
+                while (--top) {
+                    current = Math.floor(Math.random() * (top + 1));
+                    tmp = array[current];
+                    array[current] = array[top];
+                    array[top] = tmp;
+                }
+            }
+
+            return array;
+        }
+        //图片显示END
+
+
+        //日期格式
+        $scope.format = 'yyyy/MM/dd';
+        $scope.date = new Date();
+
+        //日期选择
+        $scope.today = function() {
+            $scope.dt = new Date();
+        };
+        $scope.today();
+
+        $scope.clear = function() {
+            $scope.dt = null;
+        };
+
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+            return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+        };
+
+        $scope.toggleMin = function() {
+            $scope.minDate = $scope.minDate ? null : new Date();
+        };
+
+        $scope.toggleMin();
+        $scope.maxDate = new Date(2020, 5, 22);
+
+        $scope.open1 = function() {
+            $scope.popup1.opened = true;
+        };
+
+        $scope.open2 = function() {
+            $scope.popup2.opened = true;
+        };
+
+        $scope.setDate = function(year, month, day) {
+            $scope.dt = new Date(year, month, day);
+        };
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+        $scope.altInputFormats = ['M!/d!/yyyy'];
+
+        $scope.popup1 = {
+            opened: false
+        };
+
+        $scope.popup2 = {
+            opened: false
+        };
+
+        var tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        var afterTomorrow = new Date();
+        afterTomorrow.setDate(tomorrow.getDate() + 1);
+        $scope.events =
+            [
+                {
+                    date: tomorrow,
+                    status: 'full'
+                },
+                {
+                    date: afterTomorrow,
+                    status: 'partially'
+                }
+            ];
+
+        $scope.getDayClass = function(date, mode) {
+            if (mode === 'day') {
+                var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+                for (var i = 0; i < $scope.events.length; i++) {
+                    var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+                    if (dayToCheck === currentDay) {
+                        return $scope.events[i].status;
+                    }
+                }
+            }
+
+            return '';
+        };
+        //日期选择END
+
+        //下拉按钮
+        $scope.btnitems = [
+            'The first choice!',
+            'And another choice for you.',
+            'but wait! A third!'
+        ];
+
+        $scope.status = {
+            isopen: false
+        };
+
+        $scope.toggled = function(open) {
+            $log.log('Dropdown is now: ', open);
+        };
+
+        $scope.toggleDropdown = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.status.isopen = !$scope.status.isopen;
+        };
+
+        $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
+        //下拉按钮END
+
+        //弹出层
+        $scope.divitems = ['item1', 'item2', 'item3'];
+
+        $scope.animationsEnabled = true;
+
+        $scope.open = function (size) {
+
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceCtrl',
+                size: size,
+                resolve: {
+                    items: function () {
+                        return $scope.divitems;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                console.log('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        $scope.toggleAnimation = function () {
+            $scope.animationsEnabled = !$scope.animationsEnabled;
+        };
+
+        //弹出层END
+
+
+        //分页
+        $scope.totalItems = 64;
+        $scope.currentPage = 4;
+
+        $scope.setPage = function (pageNo) {
+            $scope.currentPage = pageNo;
+        };
+
+        $scope.pageChanged = function() {
+            $log.log('Page changed to: ' + $scope.currentPage);
+        };
+
+        $scope.maxSize = 5;
+        $scope.bigTotalItems = 175;
+        $scope.bigCurrentPage = 1;
+        //分页END
+
+
+        //小贴士
+        $scope.dynamicPopover = {
+            content: 'Hello, World!',
+            templateUrl: 'myPopoverTemplate.html',
+            title: 'Title'
+        };
+
+        $scope.placement = {
+            options: [
+                'top',
+                'top-left',
+                'top-right',
+                'bottom',
+                'bottom-left',
+                'bottom-right',
+                'left',
+                'left-top',
+                'left-bottom',
+                'right',
+                'right-top',
+                'right-bottom'
+            ],
+            selected: 'top'
+        };
+        //小贴士end
+
+
+
+
+        //位置属性
+        $scope.elemVals = {};
+        $scope.parentScrollable = true;
+        $scope.parentRelative = true;
+
+        $scope.getValues = function() {
+            var divEl = $window.document.querySelector('#posdemodiv');
+            var btnEl = $window.document.querySelector('#posdemobtn');
+
+            var offsetParent = $uibPosition.offsetParent(divEl);
+            $scope.elemVals.offsetParent = 'type: ' + offsetParent.tagName + ', id: ' + offsetParent.id;
+
+            var scrollParent = $uibPosition.scrollParent(divEl);
+            $scope.elemVals.scrollParent = 'type: ' + scrollParent.tagName + ', id: ' + scrollParent.id;
+
+            $scope.scrollbarWidth = $uibPosition.scrollbarWidth();
+
+            $scope.elemVals.position = $uibPosition.position(divEl);
+
+            $scope.elemVals.offset = $uibPosition.offset(divEl);
+
+            $scope.elemVals.viewportOffset = $uibPosition.viewportOffset(divEl);
+
+            $scope.elemVals.positionElements = $uibPosition.positionElements(btnEl, divEl, 'auto bottom-left');
+        };
+        //位置属性END
+
+
+        //进度条
+        $scope.max = 200;
+
+        $scope.random = function() {
+            var value = Math.floor(Math.random() * 100 + 1);
+            var type;
+
+            if (value < 25) {
+                type = 'success';
+            } else if (value < 50) {
+                type = 'info';
+            } else if (value < 75) {
+                type = 'warning';
+            } else {
+                type = 'danger';
+            }
+
+            $scope.showWarning = type === 'danger' || type === 'warning';
+
+            $scope.dynamic = value;
+            $scope.type = type;
+        };
+
+        $scope.random();
+
+        $scope.randomStacked = function() {
+            $scope.stacked = [];
+            var types = ['success', 'info', 'warning', 'danger'];
+
+            for (var i = 0, n = Math.floor(Math.random() * 4 + 1); i < n; i++) {
+                var index = Math.floor(Math.random() * 4);
+                $scope.stacked.push({
+                    value: Math.floor(Math.random() * 30 + 1),
+                    type: types[index]
+                });
+            }
+        };
+
+        $scope.randomStacked();
+        //进度条END
+
+
+
+        //评级
+        $scope.rate = 7;
+        $scope.max = 10;
+        $scope.isReadonly = false;
+
+        $scope.hoveringOver = function(value) {
+            $scope.overStar = value;
+            $scope.percent = 100 * (value / $scope.max);
+        };
+
+        $scope.ratingStates = [
+            {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+            {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+            {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+            {stateOn: 'glyphicon-heart'},
+            {stateOff: 'glyphicon-off'}
+        ];
+        //评级END
+
+
+        //TAB
+        $scope.tabs = [
+            { title:'Dynamic Title 1', content:'Dynamic content 1' },
+            { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+        ];
+
+        $scope.alertMe = function() {
+            setTimeout(function() {
+                $window.alert('You\'ve selected the alert tab!');
+            });
+        };
+
+        $scope.model = {
+            name: 'Tabs'
+        };
+        //TAB END
+
+
+
+        //时间
+        $scope.mytime = new Date();
+
+        $scope.hstep = 1;
+        $scope.mstep = 15;
+
+        $scope.options = {
+            hstep: [1, 2, 3],
+            mstep: [1, 5, 10, 15, 25, 30]
+        };
+
+        $scope.ismeridian = true;
+        $scope.toggleMode = function() {
+            $scope.ismeridian = ! $scope.ismeridian;
+        };
+
+        $scope.update = function() {
+            var d = new Date();
+            d.setHours( 14 );
+            d.setMinutes( 0 );
+            $scope.mytime = d;
+        };
+
+        $scope.changed = function () {
+            $log.log('Time changed to: ' + $scope.mytime);
+        };
+
+        $scope.clear = function() {
+            $scope.mytime = null;
+        };
+        //时间END
+
+
+        //小贴士
+        $scope.dynamicTooltip = 'Hello, World!';
+        $scope.dynamicTooltipText = 'dynamic';
+        $scope.htmlTooltip = $sce.trustAsHtml('I\'ve been made <b>bold</b>!');
+        $scope.placement = {
+            options: [
+                'top',
+                'top-left',
+                'top-right',
+                'bottom',
+                'bottom-left',
+                'bottom-right',
+                'left',
+                'left-top',
+                'left-bottom',
+                'right',
+                'right-top',
+                'right-bottom'
+            ],
+            selected: 'top'
+        };
+        //END
+    })
+
+
+    .controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+        $scope.items = items;
+        $scope.selected = {
+            item: $scope.items[0]
+        };
+
+        $scope.ok = function () {
+            $uibModalInstance.close($scope.selected.item);
+        };
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
     })
 
 
@@ -30,6 +531,8 @@ angular.module('myApp.view5', ['ngRoute'])
         }
 
     })
+
+
 
 
 ;
